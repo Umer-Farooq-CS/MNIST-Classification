5b8a0297281e568796ae148b4cd4c98c07d7eca4
Umer Farooq <umerfarooq100888@outlook.com>
2025-04-16 12:31:23 +0500

    Completed V2

--- src/V2/neural_net.h
+++ src/V2/neural_net.h
@@ -3,24 +3,19 @@
 
 #include "nn.h"
 
-// Neural network structure
 typedef struct {
-    // Host (CPU) memory
-    double* W1;      // Flattened weights input->hidden [HIDDEN_SIZE * INPUT_SIZE]
-    double* W2;      // Flattened weights hidden->output [OUTPUT_SIZE * HIDDEN_SIZE]
-    double* b1;      // Biases for hidden layer [HIDDEN_SIZE]
-    double* b2;      // Biases for output layer [OUTPUT_SIZE]
-    
-    // Device (GPU) memory
-    double* d_W1;    // Device copy of W1
-    double* d_W2;    // Device copy of W2
-    double* d_b1;    // Device copy of b1
-    double* d_b2;    // Device copy of b2
-    
-    // Temporary buffers (device)
-    double* d_input;   // [INPUT_SIZE]
-    double* d_hidden;  // [HIDDEN_SIZE]
-    double* d_output;  // [OUTPUT_SIZE]
+    double* W1;  // Flattened [HIDDEN_SIZE * INPUT_SIZE]
+    double* W2;  // Flattened [OUTPUT_SIZE * HIDDEN_SIZE]
+    double* b1;
+    double* b2;
+    // GPU copies
+    double* d_W1;
+    double* d_W2;
+    double* d_b1;
+    double* d_b2;
+    double* d_input;
+    double* d_hidden;
+    double* d_output;
 } NeuralNetwork;
 
 // Neural network functions
@@ -31,8 +26,9 @@ void backward(NeuralNetwork* net, double* input, double* hidden, double* output,
 void train(NeuralNetwork* net, double* images, double* labels, int numImages);
 void evaluate(NeuralNetwork* net, double* images, double* labels, int numImages);
 
-// Activation functions
-void relu(double* x, int size);
-void softmax(double* x, int size);
+// Activation functions (CUDA versions)
+__global__ void matrixVectorMultiply(double* W, double* x, double* b, double* result, int rows, int cols);
+__global__ void relu_kernel(double* x, int size);
+__global__ void softmax_kernel(double* x, int size);
 
 #endif
